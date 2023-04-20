c8ce265630692e685df2ee81bce10e7a533f6581
DereckAn <dereckangeles15071998@gmail.com>
2023-04-20 14:12:24 -0600

    haciendo el boton responsive y animandolo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const [active, setActive] = useState("");
 const [toggle, setToggle] = useState(false);
 
   return (
-    <nav className={`${styles.paddindX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
+    <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
         <Link to="/" 
         className='flex items-center gap-2' 
@@ -24,7 +24,12 @@ const [toggle, setToggle] = useState(false);
 
           }}>
             <img src={logo} alt="logo" className='w-9 h-9 object-contain' />
-            <p className="text-white text-[18px] font-bold cursor-pointer">Dereck <span className='sm:block hidden'>JavaScript Mastery </span></p>
+            <p className="text-white text-[18px] font-bold cursor-pointer">
+              Dereck Angeles &nbsp; 
+                <span className='sm:block hidden'> 
+                  Full Stack Developer &nbsp;
+                </span>
+            </p>
         </Link>
 
         <ul className='list-none hidden sm:flex flex-row gap-10'>
@@ -43,12 +48,38 @@ const [toggle, setToggle] = useState(false);
           ))}
 
         </ul>
+
         <div className='sm:hidden flex flex-1 justify-end items-center'>
           <ima src={toggle ? close : menu} 
           alt="menu" 
           className='w-[28px] h-[28px] object-contain cursor-pointer'
           onClick={()=>setToggle(!toggle)} />
-          
+
+          <div className={`${!toggle ? 'hidden' : 'flex' } 
+          p-6 black-gradient absolute top-20 right-0 
+          mx-4 my-2 min-w-[140px] z-10 rounded-xl `}>
+
+            <ul className='list-none flex justify-end items-start flex-col gap-4'>
+                      {navLinks.map((Link) => (
+                        <li key={Link.id}
+                        className={`${active === Link.title 
+                          ? "text-white" 
+                          : "text-secondary"} 
+                        font-poppins font-medium cursor-pointer text-[16px]`} 
+                        
+                        onClick={ ()=> {
+                          setToggle(!toggle); //Esto es para cerrar el menu una vez que toquemos una opcion 
+                          setActive(Link.title);
+                        }}>
+                          <a href={`#${Link.id}`}>{Link.title}</a>
+                        </li>
+                      ))}
+
+                    </ul>
+
+            
+
+          </div>
         </div>
 
       </div>
